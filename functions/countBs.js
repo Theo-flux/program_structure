8a2510b73d1d2e9b1e6d65538392c4bdcd898e76
Theo-flux <72856939+Theo-flux@users.noreply.github.com>
2024-11-11 10:06:25 +0100

    functions...

---
+++ functions/countBs.js
@@ -0,0 +1,19 @@
+// Your code here.
+function countChar(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+function countBs(str) {
+    return countChar(str, "B");
+}
+
+console.log(countBs("BOB"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
